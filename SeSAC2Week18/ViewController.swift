95ccfc339a5d6828ff487689e0f7395a29447dfd
Daniel <skansmfqh7312@gmail.com>
2022-11-02 10:22:29 +0900

    Initial Commit

---
+++ SeSAC2Week18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC2Week18
+//
+//  Created by 권민서 on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SeSAC2Week18UITests/SeSAC2Week18UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SeSAC2Week18UITestsLaunchTests.swift
+//  SeSAC2Week18UITests
+//
+//  Created by 권민서 on 2022/11/02.
+//
+
+import XCTest
+
+final class SeSAC2Week18UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
